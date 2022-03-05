b45244e81729ab560655eb8868ab9d33c4f2d4a2
sosP <101015103+sosP@users.noreply.github.com>
2022-03-06 01:40:47 +0800

    Create hw0101.c

---
+++ hw0101.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+ 
+int main(){
+	int n=0,ans=0;
+	char hex[2]={0},fans[1000];
+	memset(fans,'\0',1000);
+	printf("Please enter the hex string: ");
+	for(int j=0;;j++){
+		scanf("%c%c",&hex[0],&hex[1]);
+		if(hex[0]=='0' && hex[1]=='0')
+			break;
+		
+		n=(int)strtol(hex,NULL,16);
+		for(int i=0;i<8;i++){
+			ans*=2;
+			ans+=(n>>i)%2;
+		}
+		if(isprint('\0'+ans))
+			fans[j]='\0'+ans;
+		else{
+			printf("Error\n");
+			return 0;
+		}
+	}
+	for(int j=0;;j++){
+			if(fans[j]=='\0')
+				break;
+			printf("%c",fans[j]);
+		}
+	return 0;
+}
